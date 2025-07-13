fcc9a595c3ae6163f72da6276aa6f12a1f925deb
devadh21 <devadh21@gmail.com>
2025-07-13 01:20:08 +0000

    update chatbot link

--- components/chatbot/Chatbot.tsx
+++ components/chatbot/Chatbot.tsx
@@ -9,7 +9,8 @@ export default function ChatWidget() {
     createChat({
       webhookUrl:
         
-        "https://primary-production-b3a4b.up.railway.app/webhook/4f2ca5c6-557a-46b7-9881-f085bb3f0b71/chat",
+        // "https://primary-production-b3a4b.up.railway.app/webhook/4f2ca5c6-557a-46b7-9881-f085bb3f0b71/chat",
+        "https://n8n-yhumfrlm.eu-central-1.clawcloudrun.com/webhook/4f2ca5c6-557a-46b7-9881-f085bb3f0b71/chat",
       initialMessages: [
         "Hi there! 👋",
         "Welcom to Honey Store. How can I assist you today?",
